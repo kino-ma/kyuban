25a5837f99a039d5839ce5f7f5ca16df2d83b6df
kino-ma <ma@kino.ma>
2021-11-29 18:25:07 +0900

    [feat] touch db on request for testing purpose

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,8 +1,14 @@
-from app import app
+import random
+
+from app import app, db
 from app.models import TestModel
 
 
 @app.route('/')
 def hello():
     print('hello called')
-    return 'Hello, World!'
+    model = TestModel(name=f"test{random.randint(0, 100)}")
+    db.session.add(model)
+    db.session.commit()
+    text = map(lambda m: m.name, TestModel.query.all())
+    return 'Hello, World!' + '\n' + ','.join(text)
