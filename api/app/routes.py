8e5c8f23983ed7f692767fe16d7689ab7bef1c43
kino-ma <ma@kino.ma>
2021-12-01 19:45:45 +0900

    [feat] add sample API implementation for `/thread`

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,7 @@
 import random
 
+from flask import jsonify
+
 from app import app, db
 from app.models import TestModel
 
@@ -12,3 +14,19 @@ def hello():
     db.session.commit()
     text = map(lambda m: m.name, TestModel.query.all())
     return 'Hello, World!' + '\n' + ','.join(text)
+
+
+@app.route('/thread', methods=['GET'])
+def get_thread():
+    sample_user = {
+        'id': 1,
+        'name': 'hoge',
+    }
+
+    sample_thread = {
+        'id': 1,
+        'title': 'sample thread',
+        'user': sample_user,
+    }
+
+    return jsonify(sample_thread)
