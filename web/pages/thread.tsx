f504677097fd54995c90db9291c578ebc93ed5c1
kino-ma <ma@kino.ma>
2021-12-01 19:07:24 +0900

    [feat] add blank thread page

---
+++ web/pages/thread.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ThreadProps {
+    
+}
+ 
+const Thread: React.FC<ThreadProps> = () => {
+    return (
+        <div>
+            This is thread!
+        </div>
+     );
+}
+ 
+export default Thread;
