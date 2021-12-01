3a57769d1663dd04319c4f10aeecf61337838afd
kino-ma <ma@kino.ma>
2021-12-01 19:37:17 +0900

    [feat] sample implementation of `/thread`

--- web/pages/thread.tsx
+++ web/pages/thread.tsx
@@ -1,15 +1,91 @@
-import React from 'react';
+import { NextPage } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
+
+import { UserData } from "./user";
 
 interface ThreadProps {
-    
+  thread: ThreadData;
 }
- 
-const Thread: React.FC<ThreadProps> = () => {
-    return (
-        <div>
-            This is thread!
-        </div>
-     );
-}
- 
+
+export type ThreadData = {
+  id: number;
+  title: string;
+};
+
+type ResponseData = {
+  id: number;
+  thread: ThreadData;
+  user: UserData;
+  content: string;
+};
+
+const Thread: NextPage<ThreadProps> = ({ thread }) => {
+  const user1: UserData = {
+    id: 1,
+    name: "hoge",
+  };
+  const user2: UserData = {
+    id: 2,
+    name: "fuga",
+  };
+
+  const sampleResponses: ResponseData[] = [
+    {
+      id: 1,
+      thread,
+      user: user1,
+      content: "hello thread",
+    },
+    {
+      id: 2,
+      thread,
+      user: user2,
+      content: "aaaaa",
+    },
+    {
+      id: 3,
+      thread,
+      user: user1,
+      content: "this is sample",
+    },
+    {
+      id: 3,
+      thread,
+      user: user2,
+      content: "sample response",
+    },
+  ];
+
+  const [responses, setResponses] = useState<ResponseData[]>(sampleResponses);
+  const responseItems = responses.map((response) => (
+    <li key={response.id}>
+      [{response.user.name}]: {response.content}
+    </li>
+  ));
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>thread</title>
+      </Head>
+      <main>
+        <h1>{thread.title}</h1>
+        <ul>{responseItems}</ul>
+      </main>
+    </React.Fragment>
+  );
+};
+
+Thread.getInitialProps = async (_ctx) => {
+  const thread: ThreadData = {
+    id: 1,
+    title: "Test Thread",
+  };
+
+  return {
+    thread,
+  };
+};
+
 export default Thread;
