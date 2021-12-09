14064d8aabfdc43c298c028f66396ebc55eae9be
kino-ma <ma@kino.ma>
2021-12-09 22:33:17 +0900

    [feat] add `User`

--- api/app/models.py
+++ api/app/models.py
@@ -6,9 +6,21 @@ from app import db
 
 class TestModel(db.Model):
 
+    __tablename__ = 'test_models'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now, onupdate=datetime.now)
+
+
+class User(db.Model):
+
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False,
