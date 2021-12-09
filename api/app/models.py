39768fa0a72275157e0195e54ec3d26957b97810
kino-ma <ma@kino.ma>
2021-12-09 22:57:02 +0900

    [fix] make emails nullable

--- api/app/models.py
+++ api/app/models.py
@@ -21,10 +21,14 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.now, onupdate=datetime.now)
 
     def get_all():
         return User.query.all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
