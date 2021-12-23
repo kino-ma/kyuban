2be22146a50ab5e6d4f8521acee82073e9893e7c
miki0810 <mickeymizuki@hotmail.co.jp>
2021-12-23 13:32:28 +0900

    ユーザの登録フォーム

--- web/pages/register.js
+++ web/pages/register.js
@@ -1,7 +1,36 @@
 // miki-san
+import { NextPage } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
+
 const Register = () => {
   // ここを編集する
-  return "register gamen";
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>register</title>
+      </Head>
+      <main>
+        <h1>登録画面</h1>
+        <form>
+        <label>
+          名前:<br />
+          <input type="text" name="name" />
+        </label>
+        </form>
+        <br />
+        <form>
+        <label>
+          パスワード:<br />
+          <input type="text" name="password" />
+        </label>
+        <br /><br />
+        <input type="submit" value="Submit" onClick=""/>
+        </form>
+      </main>
+    </React.Fragment>
+  );
 };
 
 export default Register;
