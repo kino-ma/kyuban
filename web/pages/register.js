356ed69e8f203d3e545bd797308214270f9a04b0
kino-ma <ma@kino.ma>
2021-12-23 14:21:38 +0900

    フォームのデータを API に送信

--- web/pages/register.js
+++ web/pages/register.js
@@ -4,7 +4,29 @@ import Head from "next/head";
 import React, { useState } from "react";
 
 const Register = () => {
-  // ここを編集する
+  const [formInput, setFormInput] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    const params = new URLSearchParams(formInput);
+
+    return fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: params
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((json) => alert(json))
+      .catch((err) => console.error(err))
+  }
 
   return (
     <React.Fragment>
@@ -13,20 +35,23 @@ const Register = () => {
       </Head>
       <main>
         <h1>登録画面</h1>
-        <form>
-        <label>
-          名前:<br />
-          <input type="text" name="name" />
-        </label>
-        </form>
-        <br />
-        <form>
-        <label>
-          パスワード:<br />
-          <input type="text" name="password" />
-        </label>
-        <br /><br />
-        <input type="submit" value="Submit" onClick=""/>
+        <form onSubmit={handleSubmit}>
+          <label>
+            名前:<br />
+            <input type="text" name="name" onChange={(e) => setFormInput({ ...formInput, name: e.target.value })} />
+          </label>
+          <br />
+          <label>
+            メールアドレス:<br />
+            <input type="email" name="email" onChange={(e) => setFormInput({ ...formInput, email: e.target.value })} />
+          </label>
+          <br />
+          <label>
+            パスワード:<br />
+            <input type="text" name="password" onChange={(e) => setFormInput({ ...formInput, password: e.target.value })} />
+          </label>
+          <br /><br />
+          <input type="submit" value="Submit" />
         </form>
       </main>
     </React.Fragment>
