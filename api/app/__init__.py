b432bdaf63b8a21ffd629b95d51d9986afc0caa7
kino-ma <ma@kino.ma>
2021-12-26 12:07:26 +0900

    [fix] allow all cors

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+CORS(app)
 
 db = SQLAlchemy()
 db.init_app(app)
 Migrate(app, db)
 
-from app import routes, models # this line must be at the bottom
+from app import routes, models  # this line must be at the bottom
