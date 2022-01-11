9439b704cc62e41f7205164658ff8e4bfbd98312
kino-ma <ma@kino.ma>
2022-01-11 20:54:29 +0900

    [refactor] add API generic wrapper: `call`

--- web/common/api.ts
+++ web/common/api.ts
@@ -15,12 +15,12 @@ type apiOptions = {
   isServerSide?: boolean;
 };
 
-export const post = (
+const call = (
+  method: "GET" | "POST" | "DELETE",
   path: string,
   data?: Record<string, string>,
   options?: apiOptions
 ): Promise<Response> => {
-  const params = data ? new URLSearchParams(data) : "";
   let { isServerSide, session } = options ?? {};
 
   if (typeof isServerSide === "undefined") {
@@ -28,13 +28,20 @@ export const post = (
   }
   const baseUrl = getBaseUrl(isServerSide);
 
-  let headers = { "Content-Type": "application/x-www-form-urlencoded" };
+  const params = data ? new URLSearchParams(data) : undefined;
+
+  let headers: HeadersInit = {};
+
+  if (data) {
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+  }
+
   if (session) {
     headers["Cookie"] = `session=${session}`;
   }
 
   const resp = fetch(`${baseUrl}${path}`, {
-    method: "POST",
+    method,
     headers,
     body: params,
     credentials: "include",
@@ -43,21 +50,14 @@ export const post = (
   return resp;
 };
 
+export const post = (
+  path: string,
+  data?: Record<string, string>,
+  options?: apiOptions
+): Promise<Response> => {
+  return call("POST", path, data, options);
+};
+
 export const get = (path: string, options?: apiOptions): Promise<Response> => {
-  let { isServerSide, session } = options ?? {};
-  if (typeof isServerSide === "undefined") {
-    isServerSide = !checkClientSide();
-  }
-  const baseUrl = getBaseUrl(isServerSide);
-
-  let headers = {};
-  if (session) {
-    headers["Cookie"] = `session=${session}`;
-  }
-
-  const resp = fetch(`${baseUrl}${path}`, {
-    credentials: "include",
-    headers,
-  });
-  return resp;
+  return call("GET", path, null, options);
 };
