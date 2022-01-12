e8908701dc105430fbfccc8c89d9519ab23daa16
kino-ma <ma@kino.ma>
2022-01-12 20:25:39 +0900

    [feat] add header items to jump links

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import Url from "node:url";
+import React from "react";
+import Link, { LinkProps } from "next/link";
+
 import styles from "../styles/Home.module.css";
+import headerStyle from "../styles/header.module.css";
+
+const HeaderItem: React.FC = (props) => {
+  return <span {...props} className={headerStyle.header__item}></span>;
+};
+
+const HeaderItemLinking: React.FC<LinkProps & { href: string }> = ({
+  href,
+  ...props
+}) => {
+  return (
+    <Link href={href}>
+      <a {...props} className={headerStyle.header__item}></a>
+    </Link>
+  );
+};
 
 const Header = () => {
   return (
     <React.Fragment>
-      <div className={styles.header}>けいじばん</div>
+      <div className={styles.header}>
+        <HeaderItemLinking href="/home">けいじばん</HeaderItemLinking>
+        <HeaderItemLinking href="/thread/create">
+          スレッドを作る
+        </HeaderItemLinking>
+      </div>
     </React.Fragment>
   );
 };
