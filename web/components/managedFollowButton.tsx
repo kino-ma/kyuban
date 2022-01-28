b8620ca11fd2342f10fddead9283f1bbe1e0bf6f
kino-ma <ma@kino.ma>
2022-01-29 03:46:33 +0900

    [feat] initialize follow state

--- web/components/managedFollowButton.tsx
+++ web/components/managedFollowButton.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { delete_, post } from "../common/api";
+import { useEffect, useState } from "react";
+import { delete_, get, post } from "../common/api";
 import { FollowData, UserData } from "../common/types";
 import { FollowButton, FollowButtonEventHandler } from "./followButton";
 
@@ -17,12 +17,21 @@ export const ManagedFollowButton = ({
     followed: false,
   });
 
+  useEffect(() => {
+    const fetchFollow = async () => {
+      const resp = await get(`/follows/${user.id}`);
+      const follow = await resp.json();
+      setFollowWithHook(follow);
+    };
+    fetchFollow();
+  }, []);
+
   const setFollowWithHook = (follow: FollowData) => {
     setFollow(follow);
     onFollowStateChange(follow);
   };
 
-  const handleFollow: FollowButtonEventHandler = async (_evt) => {
+  const handleNotFollowing: FollowButtonEventHandler = async (_evt) => {
     const uri = `/follows/${user.id}`;
     const resp = await post(uri);
     const { success } = await resp.json();
@@ -47,14 +56,12 @@ export const ManagedFollowButton = ({
     }
   };
 
-  const followedText = followed ? <p>あなたをフォローしています</p> : null;
-
   return (
     <div>
       <FollowButton
         following={following}
         onClickFollowing={handleFollowing}
-        onClickNotFollowing={handleFollow}
+        onClickNotFollowing={handleNotFollowing}
       />
     </div>
   );
